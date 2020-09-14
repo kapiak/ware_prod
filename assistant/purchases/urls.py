935f35b31c0a8f1d0514ae567f83b5ace996f9e4
Basel J. Hamadeh <hamadeh.basel@gmail.com>
2020-09-14 13:47:51 +0800

    Fixes the purchase and receive

--- assistant/purchases/urls.py
+++ assistant/purchases/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PurchaseListView
+from .views import PurchaseListView, recieve_item
 
 app_name = "purchases"
 
 urlpatterns = [
     path("", PurchaseListView.as_view(), name="purchase-order-list"),
+    path("<uuid:guid>/", recieve_item, name="receive-item"),
 ]
