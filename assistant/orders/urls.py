ae61256890834c434d2e38cc2ccacf00b638665a
Basel J. Hamadeh <hamadeh.basel@gmail.com>
2020-09-21 12:06:51 +0800

    Order Search

--- assistant/orders/urls.py
+++ assistant/orders/urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
-from .views import OrderListView, add_to_purchase_modal
+from .views import OrderListView, order_search, add_to_purchase_modal
 
 app_name = "orders"
 
 urlpatterns = [
     path("", OrderListView.as_view(), name="order-list"),
-    path("add-to-purchase-modal/<uuid:guid>/", add_to_purchase_modal, name="add_to_purchase_modal")
+    path('search/', order_search, name="order-search"),
+    path(
+        "add-to-purchase-modal/<uuid:guid>/",
+        add_to_purchase_modal,
+        name="add_to_purchase_modal",
+    ),
 ]
--- assistant/orders/views.py
+++ assistant/orders/views.py
@@ -6,6 +6,8 @@ from django.template.loader import render_to_string
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 from django.views.generic import ListView
+from django.core.paginator import Paginator
+from django.db.models import Q
 
 from assistant.products.models import ProductVariant
 from assistant.purchases.forms import PurchaseOrderForm
@@ -20,32 +22,72 @@ class OrderListView(ListView):
     paginate_by = 100
 
     def get_queryset(self):
-        qs = super().get_queryset().prefetch_related("lines", "lines__variant").select_related("user")
+        qs = (
+            super()
+            .get_queryset()
+            .prefetch_related("lines", "lines__variant")
+            .select_related("user")
+        )
+        query = self.request.GET.get('q', None)
+        if query:
+            qs = qs.filter(
+                Q(number__startswith=query)
+                | Q(metadata__shopify_id__startswith=query)
+                | Q(metadata__shopify_order_number__startswith=query)
+                | Q(lines__variant__metadata__shopify_id__startswith=query)
+            ).order_by('-created_at')
         return qs
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({'q': self.request.GET.get('q', None)})
+        return context
 
-def add_to_purchase_modal(request: HttpRequest, guid: uuid.UUID) -> HttpResponse:
+
+def order_search(request: HttpRequest) -> HttpResponse:
+    query = request.GET.get('q', None)
+    page = request.GET.get('page', None)
+    qs = Order.objects.all()
+    if query:
+        qs = Order.objects.filter(
+            Q(number__startswith=query)
+            | Q(metadata__shopify_id__startswith=query)
+            | Q(metadata__shopify_order_number__startswith=query)
+            | Q(lines__variant__metadata__shopify_id__startswith=query)
+        ).order_by('-created_at')
+    paginator = Paginator(qs, 10)
+    if page:
+        page_obj = paginator.page(page)
+    else:
+        page_obj = paginator.page(1)
+    context = {
+        'orders': page_obj.object_list,
+        'page_obj': page_obj,
+        'q': query,
+    }
+    return render(request, "orders/includes/table_list.html", context)
+
+
+def add_to_purchase_modal(
+    request: HttpRequest, guid: uuid.UUID
+) -> HttpResponse:
     template_name = "core/modal.html"
     variant = get_object_or_404(ProductVariant, guid=guid)
     context = {
         "title": _("Add To Purchase Order"),
-        "action": reverse("orders:add_to_purchase_modal", kwargs={'guid': variant.guid}),
+        "action": reverse(
+            "orders:add_to_purchase_modal", kwargs={'guid': variant.guid}
+        ),
     }
     if request.method == 'POST':
         form = PurchaseOrderForm(variant=variant, data=request.POST)
         if form.is_valid():
             form.save()
-            context.update({
-                'valid': True,
-                'title': _("Added to Purchase Order")
-            })
+            context.update(
+                {'valid': True, 'title': _("Added to Purchase Order")}
+            )
             return JsonResponse(context)
-        context.update({
-            "valid": False,
-            "form": form
-        })
+        context.update({"valid": False, "form": form})
         return render(request, template_name, context)
-    context.update({
-        "form": PurchaseOrderForm(variant=variant)
-    })
+    context.update({"form": PurchaseOrderForm(variant=variant)})
     return render(request, template_name, context)
